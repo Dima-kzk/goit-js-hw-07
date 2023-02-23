e84871fe3dc0772e6d36ad8ef035c6e956abc4d2
Dima-kzk <dimakzk@yahoo.com>
2023-02-23 09:51:37 +0200

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+
+function cretTegimg({preview, description, original}){
+    return `
+    
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    
+    `;
+}
+
+function render(galleryItems){
+    gallery.innerHTML = galleryItems.map(cretTegimg).join('');
+}
+
+render(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a',
+{
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+});
