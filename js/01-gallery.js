a3f9811c997ac7e7a6e02ef0c12b50af9f9f6eda
Dima-kzk <dimakzk@yahoo.com>
2023-02-23 02:12:23 +0200

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+
+function cretTegimg({preview, description, original}){
+    return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>
+    `;
+}
+
+function render(galleryItems){
+    gallery.innerHTML = galleryItems.map(cretTegimg).join('');
+}
+
+render(galleryItems);
+
+
+
+gallery.addEventListener("click", event=>{
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") return;
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    `);
+    
+    instance.show();
+
+    document.addEventListener("keydown", onKeyDown);
+
+    function onKeyDown(event){
+        if(event.code === "Escape") instance.close();
+        document.removeEventListener("keydown", onKeyDown);
+    }
+});
+
+
